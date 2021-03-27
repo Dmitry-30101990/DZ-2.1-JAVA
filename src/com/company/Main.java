b58e35f68b1e08a28a80cffcfd7a64f47fd64fff
Dmitry Zhmakin <dima8837922@gmail.com>
2021-03-27 21:59:25 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int count = 2_000_000_000;
+	int transfer = 500_000_000;
+	int balance = count + transfer;
+	System.out.println(balance);
+    }
+}
